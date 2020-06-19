14d8be20917c2120d546c3b0837217b357e8fc98
= <=>
2020-06-19 19:46:40 +0300

    Added objects for user data in sign up JS

--- JS/signupscript.js
+++ JS/signupscript.js
@@ -1,7 +1,7 @@
 // Fundamental JS for validating sign up detials
 
-const id = document.getElementById('email');
-const names = document.getElementById('password');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
 
@@ -9,12 +9,26 @@ const errorElement = document.getElementById('error');
 
 form.addEventListener('btn', (e) => {
     let messages = [];
-    if (names.value === '' || names.value == null) {
+    if (email.value === '' || email.value == null) {
         messages.push('Name is required');
     }
     if (messages.length > 0) {
         e.preventDefault();
         errorElement.innerText = messages.join(', ');
     }
-
 })
+
+// USER ACCOUNTS STORED BELOW--->
+// Declaring user objects: email
+
+email = {
+    "emails": []
+}
+
+// password
+
+password = {
+    "passwords": []
+}
+
+// USER ACCOUNTS ABOVE--->
