4e2f1b314de79fca6337bb1c9dd4dbb34480a557
= <=>
2020-06-20 11:57:38 +0300

    added validation

--- JS/signupscript.js
+++ JS/signupscript.js
@@ -14,27 +14,32 @@ form.addEventListener('btn', (e) => {
     if (email.value === ' ' || email.value == null) {
         messages.push('Name is required');
     }
+
+    if (password.value.length <= 8) {
+        messages.push('Password must be longer than 8 characters')
+    }
+
     if (messages.length > 0) {
-        e.preventDefault();
-        errorElement.innerText = messages.join(', ');
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
     }
 })
 
 
 // EMAIL VALIDATION
-function ValidateEmail(inputText) {
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (inputText.value.match(mailformat)) {
-        document.form1.text1.focus();
-        return true;
-    } else {
-        alert("You have entered an invalid email address!");
-        document.form1.text1.focus();
-        return false;
-    }
-}
+// function ValidateEmail(inputText) {
+//     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     if (inputText.value.match(mailformat)) {
+//         document.form1.text1.focus();
+//         return true;
+//     } else {
+//         alert("You have entered an invalid email address!");
+//         document.form1.text1.focus();
+//         return false;
+//     }
+// }
 
-
+z
 // USER ACCOUNTS STORED BELOW--->
 // Declaring user objects: email
 
@@ -49,3 +54,14 @@ password = {
 }
 
 // USER ACCOUNTS ABOVE--->
+
+//CAPTURE USER DATA
+
+function captureUserData(e, p, check) {
+    if (check == true) {
+        email.value.push(email);
+        password.value.push(password);
+        return true;
+    }
+    return false;
+}
