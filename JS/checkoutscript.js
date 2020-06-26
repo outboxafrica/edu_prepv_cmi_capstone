d7206ffe0c6a76dd60e1654c72f7b368d5c1bf50
= <=>
2020-06-26 07:56:00 +0300

    checkout page js

--- JS/checkoutscript.js
+++ JS/checkoutscript.js
@@ -85,7 +85,7 @@ var x = setInterval(function() {
 
     // Display the result in the element with id="demo"
     document.getElementById("timer").innerHTML = "You have " + days + "DAYS " + hours + "h " +
-        minutes + "m " + seconds + "s  to return your movies!";
+        minutes + "m " + seconds + "s <br> to return your movies!";
 
     // If the count down is finished, write some text
     if (distance < 0) {
