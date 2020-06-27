a9aec7fa20caee465c55fbf8e57da72129d2db56
= <=>
2020-06-27 18:16:07 +0300

    sign up validation

--- JS/signupscript.js
+++ JS/signupscript.js
@@ -1,19 +1,23 @@
+//checkbox
+
+
 function formvalidation() {
-    var uemail = document.signupform.email;
-    var passid = document.signupform.password;
-    var ufirst = document.signupform.confirmpassword;
-    var check = document.signupform.check;
-    alert("check is " + check.value);
+    var uemail = document.getElementById("email");
+    var passid = document.getElementById("password");
+    var ufirst = document.getElementById("confirmPassword");
+    var check = ch.value;
+
+    //return values from checkvalidation
+    alert("check is " + check);
 
     /**Invoking functions with arguments */
-    if (emailvalidation(uemail)) {
-        if (passid_validation(passid, 8, ufirst)) {
-
-            // alert('Welcome to Magi-Cinema ' + uemail.value)
-
-            return true;
-            // alert("Form Submitted Succesfully");
-            // window.location.reload(Get);
+    if (checkValidation(check)) {
+        if (emailvalidation(uemail)) {
+            if (passid_validation(passid, 7, ufirst)) {
+                alert('Welcome to Magi-Cinema ' + uemail.value);
+                window.location.href = "movies.html";
+                return true;
+            }
         }
     }
     return false;
@@ -24,7 +28,13 @@ function emailvalidation(uemail) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (uemail.value.match(mailformat)) {
         return true;
+    } else if (uemail.value == null || uemail.value == '') {
+
+        alert("Please enter an email address!");
+        uemail.focus();
+        return false;
     } else {
+
         alert("You must enter a valid email address!");
         uemail.focus();
         return false;
@@ -36,16 +46,43 @@ function emailvalidation(uemail) {
 function passid_validation(passid, mx, ufirst) {
     //Accessing form element
     var passid_len = passid.value.length;
-    if (passid_len == 0 || passid_len <= mx || passid.value == null) {
+    if (passid_len == 0 || passid.value == null) {
+        alert("Please input a password");
+        passid.focus();
+
+        return false;
+    } else if (passid_len < mx) {
         alert("Password characters should be at least 8 characters!");
         passid.focus();
-        if (passid.value != ufirst.value) {
-            alert("Your passwords should match!");
-            return false;
-        }
+        return false;
+    } else if (passid.value != ufirst.value) {
+        alert("Both passwords should match!");
+        passid.focus();
+        return false;
     }
     return true;
 }
+
+function checkValidation(box) {
+    if (box == "true") {
+        return true;
+    } else {
+        alert("Please check the box to continue");
+        return false;
+    }
+}
+
+//validate the checkbox with boolean logic
+const ch = document.getElementById('checkbox');
+ch.addEventListener('click', function() {
+    console.log(ch.value);
+
+    if (ch.value != "true") {
+        ch.value = true;
+    } else if (ch.value == "true") {
+        ch.value = false;
+    }
+});
 
 function openSlideMenu() {
     document.getElementById('side-menu').style.width = '250px';
@@ -67,3 +104,5 @@ submit_buttonLink.addEventListener('click', function() {
         event.preventDefault()
     }
 })
+
+//adding button listener for checkbox
